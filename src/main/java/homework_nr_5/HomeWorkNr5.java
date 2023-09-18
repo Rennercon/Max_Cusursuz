af51a3770627c580eb557113e2559ad0c27df130
Maxim <maximklcusursuz@gmail.com>
2023-09-18 14:46:29 +0300

    Removed all comments in HomeWork5

--- src/main/java/homework_nr_5/HomeWorkNr5.java
+++ src/main/java/homework_nr_5/HomeWorkNr5.java
@@ -1,8 +1,6 @@
 package homework_nr_5;
-import java.math.BigInteger;
 public class HomeWorkNr5 {
     public static void main(String[] args) {
-        // Инициализация и заполнение массива
         int[] numbers = new int[100];
         int x = numbers.length;
         for (int i = 0; i != numbers.length; i++) {
@@ -14,34 +12,22 @@ public class HomeWorkNr5 {
         int sum = 0;
         long product1 = 1;
         for (int i = 0; i != 100; i++) {
-            // Сумма Четных
             if (numbers[i] % 2 == 0) {
                 sum += numbers[i];
             }
-            // Произвдение нечетных
             if (numbers[i] % 2 != 0 ) {
                 product1 *= numbers[i];
             }
         }
-        //Ввывод результатов
         System.out.println();
         System.out.println("Сумма четных = " + sum);
         System.out.println("Произвдение нечетных = " + product1);
 
 
-        // Проверка для умножения при использовании типа long
         long prod = 1;
         for (int i = 1; i <= 100; i += 2) {prod *= i;}
         System.out.println("Произведение всех нечетных чисел от 1 до 100: " + prod);
 
-        //правильный ответ для умножения всех нечетных чисел с использованием класса BigInteger
-//        BigInteger product = new BigInteger("1");
-//        for (int i = 1; i <= 100; i += 2) {
-//            product = product.multiply(BigInteger.valueOf(i));
-//        }
-//        System.out.println(product);
-
-        // Копирование numbers в copyNumbers
         int[] copyNumbers = new int[numbers.length];
         for (int i = 0; i != numbers.length; i++) {
             copyNumbers[i] = numbers[i];
