99a2d6a838d99a78276e9f91863fec0fc4dba1ba
Maxim <maximklcusursuz@gmail.com>
2023-10-26 12:12:41 +0300

    implements Serializable all class

--- src/main/java/homework_nr_14/Vehicle.java
+++ src/main/java/homework_nr_14/Vehicle.java
@@ -1,9 +1,10 @@
 package homework_nr_14;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Vehicle {
+public class Vehicle implements Serializable {
     private String name;
     private final List<Vehicle> vehicleList = new ArrayList<>();
 
