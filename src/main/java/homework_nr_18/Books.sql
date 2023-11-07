dd63a7fbeed5a54c5d0501aa14ce6cb058dcecd1
Maxim <maximklcusursuz@gmail.com>
2023-11-07 14:07:33 +0200

    Finished homework_nr_18

---
+++ src/main/java/homework_nr_18/Books.sql
@@ -0,0 +1,22 @@
+create database Books_db;
+
+create table Books(
+	book_id serial primary key,
+	name text,
+	author text,
+	date_of_publishing date,
+	genre text,
+	available_count int
+);
+
+insert into Books (name, author, date_of_publishing, genre, available_count)
+values
+('The Great Gatsby', 'F. Sco^ Fitzgerald', '1925-01-01', 'Classics', 3),
+('To Kill a Mockingbird', 'Harper Lee', '1960-01-01', 'Ficnon', 5),
+('1984', 'George Orwell', '1949-01-01', 'Science Ficnon', 2),
+('Pride and Prejudice', 'Jane Austen', '1813-01-01', 'Romance', 4);
+
+select name, author, date_of_publishing, genre, available_count
+from Books;
+
+select * from Books;
